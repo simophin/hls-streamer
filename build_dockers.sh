13d13629c399980cbee055cb308160f137599ad5
simophin <dev@fanchao.nz>
2022-01-07 13:38:34 +1300

    Build different docker images

--- build_dockers.sh
+++ build_dockers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for variant in vaapi ubuntu scratch nvidia alpine centos; do
-  set NAME=$REGISTRY/$IMAGE_NAME:$variant
-  echo Buildling $NAME
+  local NAME=$REGISTRY/$IMAGE_NAME:$variant
+  echo Building $NAME
   docker build --build-arg FFMPEG_VARIANT=3.4-$variant . -t $NAME
   docker push $NAME
 done
